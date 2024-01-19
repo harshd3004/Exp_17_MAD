cf84bfc3e37c5c144bbdbbbad8e91bb8783ae777
lenovo 03 <desaiharsh512@gmail.com>
2024-01-19 15:53:49 +0530

    implemented life cycle methods

--- app/src/main/java/com/example/exp_17_mad/HelloWorld.java
+++ app/src/main/java/com/example/exp_17_mad/HelloWorld.java
@@ -3,6 +3,7 @@ package com.example.exp_17_mad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloWorld extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class HelloWorld extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+        Log.d("Activity Lifecycle","onCreate invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Acticity Lifecycle","onStart invoked");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Activity Lifecycle","onResume invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Activity Lifecycle","onPause invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Activity Lifecycle","onStop invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
